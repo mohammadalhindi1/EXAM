e16763f864a54fdc9e85f94f301c656d6ec3675f
Mohammad AlHindi <94603043+mohammadalhindi1@users.noreply.github.com>
2025-11-28 00:52:01 +0300

    Add files via upload

--- leval1/rot_13.c
+++ leval1/rot_13.c
@@ -12,16 +12,20 @@
 
 #include <unistd.h>
 
-int main(int ac, char *av[])
+int	main(int ac, char *av[])
 {
-	if(ac == 2)
+	int	i;
+
+	if (ac == 2)
 	{
-	int i = 0;
-		while(av[1][i])
+		i = 0;
+		while (av[1][i])
 		{
-			if((av[1][i] > 64 && av[1][i] < 78) || (av[1][i] > 96 && av[1][i] < 110))
+			if ((av[1][i] > 64 && av[1][i] < 78) || (av[1][i] > 96
+					&& av[1][i] < 110))
 				av[1][i] += 13;
-			else if((av[1][i] > 77 && av[1][i] < 91) || (av[1][i] > 109 && av[1][i] < 123))
+			else if ((av[1][i] > 77 && av[1][i] < 91) || (av[1][i] > 109
+						&& av[1][i] < 123))
 				av[1][i] -= 13;
 			write(1, &av[1][i], 1);
 			i++;
