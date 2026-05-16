74bdbc6170b70943cb574658722b489d5d0e95cb
Mohammad AlHindi <94603043+mohammadalhindi1@users.noreply.github.com>
2026-05-16 17:49:14 +0300

    Add files via upload

---
+++ EXAM-main/Exam02/leval3/rstr_capitlizer.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+
+int main(int ac , char *av[])
+{
+	if(ac == 2)
+	{
+		int i =0;
+		while(av[1][i])
+		{
+			if(av[1][i] >= 'A' && av[1][i] <= 'Z')
+				av[1][i] += 32;
+			i++;
+		}
+		i = 0;
+		while(av[1][i])
+		{
+			if ((av[1][i] >= 'a' && av[1][i] <= 'z') &&
+				!(av[1][i + 1] >= 'a' && av[1][i + 1] <= 'z'))
+				av[1][i] -= 32;
+			write(1, &av[1][i],1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+}
